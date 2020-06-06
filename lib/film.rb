2903be963edfe24da961ff8e7e5ad19433d4bc25
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-06-06 13:38:28 -0400

    recommit

--- lib/film.rb
+++ lib/film.rb
@@ -27,25 +27,7 @@ class Film
     end 
 
 
-            #     doc.at("h2:contains(text(), '#{film.name}')")
 
-        #     # doc.at('h2:contains("The Tall T")').next.next.next.next (url in here)
-    # def self.scrape_bio_from_title(movie)
-    #     url = "https://www.loc.gov/programs/national-film-preservation-board/film-registry/descriptions-and-essays/"
-    #     index = Film.all.index{|x| x.name.downcase == movie}
-    #     film = find_by_idx(index)
-    #     doc = Nokogiri::HTML(open(url))
-    #     body = doc.css('article')
-    #     binding.pry
-    #     if !!doc.at_css("p:contains('#{film.name}')")
-    #         film.desc = doc.at_css("p:contains('#{film.name}')").text.strip
-    #     else 
-    #         film.desc = "No description available"
-    #     end 
-    #     if !!doc.at_css("p:contains('#{film.name}')")
-    #         film.url = "https://www.loc.gov" +  doc.at_css("p:contains('#{film.name}')").css('a').attribute('href')
-    #     end 
-    # end 
 
     def self.search(query)
         term = query.downcase.strip
