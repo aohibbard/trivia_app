7b7e686b5ce3a721d38017d6dd942c660d103cd7
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-11-01 16:04:51 +0000

    fixes menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,13 +4,16 @@ class Menu
     @@questions = []
 
     def run 
+        # initialize Clue class
         Clue.get_trivia
+        puts
+        puts
         puts Rainbow("WELCOME!").magenta
-        puts "This trivia program helps you put your trivia knowledge to the test"
-        puts "so you can rise to the top in any Slack group, pub quiz, or Trivial Pursuit game"
+        puts "Let's put your trivia knowledge to the test!"
         sleep 1
-        puts "Each game consists of ten multiple choice questions"
-        puts "At any point, you can exit the game by entering " + Rainbow("quit").red
+        puts
+        puts "Each game consists of ten multiple choice questions."
+        puts "At any point, you can exit the game by entering " + Rainbow("'quit'").red + "."
         puts "Just enter " + Rainbow("'start'").green + " to play."
         get_input
     end 
@@ -31,18 +34,24 @@ class Menu
     end 
 
     def start_game
+        # rest all variables
         @@counter = 0
         @@score = 0
+
+        # create an array of ten random numbers for indices of clue
         randoms = Set.new
         while randoms.size < 10 
             randoms.add(rand(0..19))
         end
         @@questions = randoms.to_a
+
+        puts Rainbow("LET'S GO!").aqua
         next_question
     end 
 
     def next_question
         # find question index
+        # after each question, index 0 is removed, so next question is always at index 0
         i = @@questions[0]
         @@counter += 1
 
@@ -50,36 +59,52 @@ class Menu
         answers = Clue.all[i].all_answers.shuffle
         correct_idx = answers.index(Clue.all[i].answer)
 
-        puts Rainbow("#{@@counter}.").magenta + " #{Clue.all[i].question}"
+        puts Rainbow("#{@@counter}. #{Clue.all[i].question}").blue
         sleep 1
-        answers.each_with_index{|item, idx| puts "#{idx}. #{item}"}
+        answers.each_with_index{|item, idx| puts "#{idx + 1}. #{item}"}
         puts Rainbow("Choose the number next to the answer you think is correct.").magenta
 
-        check_answer(correct_idx)
+        check_answer(correct_idx, i)
+    end
+
+    def is_valid(entry)
+        if entry == 'quit'
+            quit
+        end 
+        valids = [1, 2, 3, 4]
+        if valids.include?(entry.to_i)
+            return true
+        else
+            return false
+        end 
     end 
 
-    def check_answer(answer)
+    def check_answer(answer, idx)
         input = gets.chomp.downcase
-        if input == 'quit'
-            quit
-        else
-            input = input.to_i()
+        if is_valid(input)
+            input = input.to_i() - 1
             answer = answer.to_i()
             if input == answer
                 puts Rainbow("You are correct!").green
-                puts ""
+                puts 
                 @@score += 1 
             else 
                 puts Rainbow("That's not correct.").red
-                puts "The correct answer is #{answer}"
-                puts ""
+                puts "The correct answer is: " + Rainbow("#{answer + 1}. #{Clue.all[idx].answer}").cyan
+                puts 
             end
+            puts "Your current score is " + Rainbow("#{@@score}/10") 
+            puts
             if @@counter >= 10
                 tally_score
             else 
                 @@questions.shift()
                 next_question
             end 
+        else 
+            puts Rainbow("Please enter a valid input.").red
+            puts 
+            check_answer(answer, idx)
         end
     end 
 
@@ -95,13 +120,14 @@ class Menu
         elsif @@score == 10
             puts "Perfect score!"
         end 
-        puts "Want to play again? Just enter 'start'."
-        puts "Or quit to exit."
+        puts "Want to play again? Just enter " + Rainbow("'start'.").green
+        puts "Or " + Rainbow("'quit'").red + " to exit."
         get_input
     end
 
     def quit
         puts "Thanks for playing!"
+        puts
         exit
     end 
 
