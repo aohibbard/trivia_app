665ca0fe28ae5829e8c279c7f56f4b49d707fe23
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-11-03 15:07:57 +0000

    finishes tests for clue

--- spec/clue_test.rb
+++ spec/clue_test.rb
@@ -2,13 +2,18 @@ require 'clue'
 
 describe Clue do 
     before do 
-        # @first_clue = Clue.new(:wrong_answers => ['90', '115', '50'])
-        @first_clue = Clue.new(:question => "How many folds does a chef's hat have?", :answer => 100, :wrong_answers => [90, 115, 50])
+        Clue.all.clear()
+        new_clue = Hash["question" => "How many folds does a chef's hat have?", "answer" => 100, "wrong_answers" => [90, 115, 50]]
+        @first_clue = Clue.new
+        @first_clue.question = new_clue['question']
+        @first_clue.answer = new_clue['answer'] 
+        @first_clue.wrong_answers = new_clue['wrong_answers']
+        @first_clue.all_answers = [new_clue['answer']].concat(new_clue['wrong_answers'])
     end 
 
     describe "wrong answers" do 
         it "includes an array of wrong answers" do 
-            expect(@first_clue.wrong_answers.class).to eq(Hash || Array)
+            expect(@first_clue.wrong_answers.class).to eq(Array)
         end 
     end 
 
@@ -20,7 +25,13 @@ describe Clue do
     
     describe "all" do 
         it "is added to all" do 
-            expect(Clue.all[0]).to eq(@first_clue)
+            expect(Clue.all.size).to eq(1)
         end
+    end 
+
+    describe "all answers" do 
+        it "includes wrong answers" do 
+            expect(@first_clue.all_answers).to include(@first_clue.wrong_answers[0])
+        end 
     end 
 end 
